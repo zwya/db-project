d2070a7d40ced411be9885b50c1bae9ad69b4b61
Ahmed Hanie <zwya@ymail.com>
2018-06-28 11:02:06 +0200

    Seed Project

---
+++ src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+
+  book = {title: "hello", description: "hello", isbn: "hello", author: "hello", publisher: "hello", published_year: "hello", updated_date: "hello", _id: "hello"};
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
+
+  ngOnInit() {
+    this.getBookDetails(this.route.snapshot.params['id']);
+  }
+
+  getBookDetails(id) {
+    this.api.getBook(id)
+      .subscribe(data => {
+        console.log(data);
+        this.book = data;
+      });
+  }
+
+  deleteBook(id) {
+    this.api.deleteBook(id)
+      .subscribe(res => {
+          this.router.navigate(['/books']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+}
