4728040e654301213af15d83820578c6952c3c61
Ahmed Hanie <zwya@ymail.com>
2018-06-30 07:31:19 +0200

    Added bootstrap, jquery and popper and added skeleton navbar html code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
